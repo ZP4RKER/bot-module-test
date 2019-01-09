0dbb9457062b38764ef35e77e9c274ca0319da9b
zpdev <acc@zpdev.co>
2019-01-09 17:21:20 +1100

    commit

---
+++ src/main/java/com/zp4rker/bots/moduletest/Main.java
@@ -0,0 +1,19 @@
+package com.zp4rker.bots.moduletest;
+
+import com.zp4rker.bots.modularitytest.Module;
+
+/**
+ * @author ZP4RKER
+ */
+public class Main extends Module {
+
+    public Main() {
+        super("Test Module", "v1.0");
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+}
